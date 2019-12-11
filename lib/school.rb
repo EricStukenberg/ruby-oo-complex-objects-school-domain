7c96bc36310b9b6b38a529713015696928855f86
github username <github email address>
2019-12-11 14:09:49 -0800

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
 # code here!
+require 'pry'
+class School
+    def initialize(name)
+        @school_name = name
+        @roster = Hash.new
+    end 
+    attr_reader :roster, :grade
+    
+    def add_student(student, grade)
+        @grade = grade
+        array = Array.new
+       # binding.pry
+        if(self.roster[grade] == nil)
+            array.push(student)
+            self.roster[grade] = array
+        else
+            array = roster[grade]
+            array.push(student)
+            self.roster[grade] = array
+
+        end
+
+    end 
+
+    def grade(grade)
+
+        return self.roster[grade]
+    end 
+
+    def sort() 
+        self.roster.each {|key, value| 
+            value.sort!
+        } 
+        return  self.roster
+        
+    end 
+end 
